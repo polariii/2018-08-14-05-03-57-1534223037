8fd4caa5ce9997fab5568acaae44c42b67553437
polariii <polaryw@gmail.com>
2018-09-24 14:46:40 +0800

    sum

--- main/main.js
+++ main/main.js
@@ -1,3 +1,9 @@
-module.exports = function main() {
-  // Write your code here
+module.exports = function main(input) {
+  var num = input.split('');
+  var arr = num.map(Number);
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++){
+    sum += arr[i]
+  };
+  return sum;
 };
